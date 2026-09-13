a016ff237afd0f204ecd7437464865bea1dcccf4
YStech-arch <ystech78@gmail.com>
2026-09-13 21:26:35 +0700

    feat: add main entry point with YS Tech ASCII banner

---
+++ main.py
@@ -0,0 +1,12 @@
+print("""
+__   ______   _____ECH 
+\ \ / / ___| |_   _|___  ___| |__  
+ \ V /\___ \   | |/ _ \/ __| '_ \ 
+  | |  ___) |  | |  __/ (__| | | |
+  |_| |____/   |_|\___|\___|_| |_|
+
+ Created by : YS Tech <ystech78@gmail.com>
+ Project    : Hybrid Music Bot
+""")
+
+print("[INFO] Bot Yuki sedang berjalan...")
